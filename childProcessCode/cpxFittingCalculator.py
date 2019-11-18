3b45de7d9c8dc33f819f5c3b942d01cf469d6dbb
James Fleming <jf13g13@gmail.com>
2019-11-18 17:36:30 +0000

    inital commit

---
+++ childProcessCode/cpxFittingCalculator.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.optimize import curve_fit
+import pandas as pd
+import sys
+
+#try:
+#
+ionsNamesInput = list((sys.argv[1].split(",")))
+partitionCoefficientsInput = np.array((sys.argv[2].split(","))).astype(np.float)
+
+
+##    check that they are the same length
+#except IndexError:
+#ionsNamesInput = ["Lu","Yb","Er", "Y", "Dy","Tb"	,"Gd",	"Eu", "Sm",	"Nd",	"Pr",	"Ce",	"La"]
+#partitionCoefficientsInput = [0.820255866	,0.818727698,	0.884169946,	0.891499641,	0.894813905,	0.868754382	,0.823718945	,0.628083738	,0.658927043,	0.435905699,	0.333214165,	0.234228321,	0.155300619]
+
+
+ionDf = pd.DataFrame ( {'ion': ['La',
+'Ce',
+'Pr',
+'Nd',
+'Sm',
+'Eu',
+'Gd',
+'Tb',
+'Dy',
+'Ho',
+'Er',
+'Tm',
+'Yb',
+'Lu','Y'], 'radii': [1.160,
+1.143,
+1.126,
+1.109,
+1.079,
+1.066,
+1.053,
+1.040,
+1.027,
+1.015,
+1.004,
+0.994,
+0.985,
+0.977,
+1.019]})
+
+inputDf = pd.DataFrame({'ion':ionsNamesInput,'DCpxLiquid':partitionCoefficientsInput})
+final = pd.merge(ionDf, inputDf, how='inner', left_on="ion", right_on="ion")
+final.sort_values(by= "radii",inplace=True)
+
+def blundy_func(r_i,D_o,E,r_o):
+    inner =  ((-4 * np.pi * E * 6.0221409*10**(23) * (r_o/2) * (r_i   - r_o )**2 + 1/3 * (r_i - r_o)**3))  / (8.314*1448)
+    #print('inner array')
+    #print (inner)
+    return D_o * np.exp(inner)
+
+# =============================================================================
+# Attempt to fit parameters to the data
+# =============================================================================
+
+#Initial parameter guesses for parameters
+p0=[1.092110848188546,273624000000,2*10**(-11)]
+upper  = [np.inf,np.inf,1.02189e-10]
+lower = [-1000,0.0,0.0]
+boundary =(lower,upper)
+
+r_i_fit = np.array(final["radii"])
+
+popt, pcov = curve_fit(blundy_func, r_i_fit* 10**(-10), np.array(final["DCpxLiquid"]),p0,maxfev=100000)
+
+
+residuals = np.array(final["DCpxLiquid"]) - blundy_func(r_i_fit* 10**(-10), *popt)
+ss_res = np.sum(residuals**2)
+ss_tot = np.sum((np.array(final["DCpxLiquid"])-np.mean(np.array(final["DCpxLiquid"])))**2)
+r_squared = 1 - (ss_res / ss_tot)
+
+resultsDict = {"Do":popt[0],"E":popt[1],"Ro":popt[2],"R Squared":r_squared}
+
+
+print (resultsDict)
+
+
+
---
+++ src/runServer.ts
@@ -0,0 +1,107 @@
+const express = require( "express" );
+const app = express();
+const port = 8080; // default port to listen
+import { Request, Response, NextFunction } from 'express';
+const spawn = require("child_process").spawn;
+var cors = require('cors');
+var bodyParser = require('body-parser')
+
+app.use("/views",express.static(__dirname +"/../views/"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let allowedOrigins = ["http://localhost:8080"]
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      console.log(origin)
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// allow access from client app
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS,   PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Only uncomment the next line if you find the idea of chopping your own ball
+  // s off fun
+  // res.setHeader('Content-Type', 'text/plain');
+  // Set to true if you need the website to include cookies in the   requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Pass to next layer of middleware
+  next();
+});
+
+
+// define a route handler for the default home page
+app.get( "/", ( req :Request, res : Response ) => {
+    res.render(__dirname + '/../views/index.ejs')
+    
+} );
+
+
+app.get( "/submitjob", ( req :Request, res : Response ) => {
+    
+    res.render(__dirname + '/../views/form.ejs')
+} );
+
+app.post("/pyroxeneFitting", (req:Request,res:Response)=> {
+    var arg1 = req.body['ionList']
+    var arg2 = req.body['partitionCoefficients']
+    const pythonProcess = spawn('python3',["../childProcessCode/cpxFittingCalculator.py", arg1, arg2]);
+
+    pythonProcess.stdout.on('data', (data : any) => {
+        console.log(data.toString())
+        
+        var resultString = data.toString().replace(/'/g, '"')
+        
+        console.log(resultString)
+        var results = JSON.parse(resultString)
+        res.json(results).send()
+        
+        // Do something with the data returned from python script
+    })
+
+    pythonProcess.stderr.on('data', (data :any) => {
+        console.log("Python error:")
+        console.log(`stderr: ${data.toString()}`);
+        res.render(__dirname + '/../views/error.ejs',{
+            errorMesssage: data.toString()
+        })
+    });
+})
+
+
+
+app.post("/test123", (req:Request,res:Response)=> {
+    console.log(req.body['ionList'])
+    console.log(req.body['partitionCoefficients'])
+    
+
+    res.send("worked")
+})
+
+app.get('*', function(req, res){
+    res.status(404);
+    res.send("sorry missed")
+    // res.render(__dirname + '/../../views/404.ejs');
+  });
+
+
+// start the Express app
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
